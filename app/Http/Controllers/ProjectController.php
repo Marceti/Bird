1c9b3d76803eb12b606d4e82e074f6e339f4a1cd
vagrant <vagrant@bird>
2019-01-20 13:48:19 +0000

    project creation validation test

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,25 +5,28 @@ namespace App\Http\Controllers;
 use App\Project;
 use Illuminate\Http\Request;
 
-class ProjectController extends Controller
-{
+class ProjectController extends Controller {
 
     public function index()
     {
         //TODO:filter result
-        $projects=Project::all();
+        $projects = Project::all();
+
         //TODO:show filtered projects
-        return view('projects.index',compact('projects'));
+        return view('projects.index', compact('projects'));
     }
-    
-    
+
+
     public function store()
     {
-        //TODO:validate
-        
+        //validate
+        request()->validate([
+            'title'=>'required',
+            'description'=>'required'
+        ]);
 
         //persist
-        Project::create(request()->only(['title','description']));
+        Project::create(request()->only(['title', 'description']));
 
         //redirect
         return redirect()->route('projects');
--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -6,31 +6,54 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ProjectTest extends TestCase
-{
+class ProjectTest extends TestCase {
+
     use WithFaker, RefreshDatabase;
 
 
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        //Given
+        $projectData = [
+            'title'       => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        //When
+        $response = $this->post('/projects', $projectData);
+
+        //Then
+        $response->assertRedirect('/projects');
+        $this->assertDatabaseHas('projects', $projectData);
+        $this->get('/projects', $projectData)->assertSee($projectData['title']);
+    }
+
      /** @test */
-         public function a_user_can_create_a_project()
+         public function a_project_must_have_a_title()
          {
-             $this->withoutExceptionHandling();
-
              //Given
-            $projectData=[
-              'title'=>$this->faker->sentence,
-              'description'=>$this->faker->paragraph
-            ];
+            $attributes=factory('App\Project')->make(['title'=>'']);
 
-            //When
-            $response = $this->post('/projects',$projectData);
+             //when
+             $response = $this->post('/projects',$attributes->toArray());
 
              //Then
-
-             $response->assertRedirect('/projects');
-           $this->assertDatabaseHas('projects',$projectData);
-           $this->get('/projects',$projectData)->assertSee($projectData['title']);
-
-
+             $response->assertSessionHasErrors('title');
          }
+
+    /** @test */
+    public function a_project_must_have_a_description()
+    {
+        //Given
+        $attributes=factory('App\Project')->make(['description'=>'']);
+
+        //when
+        $response = $this->post('/projects',$attributes->toArray());
+
+        //Then
+        $response->assertSessionHasErrors('description');
+    }
 }
