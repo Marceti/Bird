861b6168e83488a326980fead7b7df59cdb522be
vagrant <vagrant@bird>
2019-02-07 19:42:33 +0000

    project index ordered

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller {
 
     public function index()
     {
-        $projects = auth()->user()->projects;
+        $projects = auth()->user()->projects()->latest('updated_at')->get();
 
         return view('projects.index', compact('projects'));
     }
